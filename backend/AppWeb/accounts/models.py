8e02241dc4011e2d2bd8d1e911a37f04550a21fc
solansh <solanshmontoyamm@gmail.com>
2019-06-26 17:10:38 -0500

    cua cua

--- backend/AppWeb/accounts/models.py
+++ backend/AppWeb/accounts/models.py
@@ -6,13 +6,13 @@ from .helpers import profile_image_path
 class Profile(models.Model):
     """An extension of user model."""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone = models.CharField(max_length=12)
-    cant_km = models.IntegerField('cantidad de km', null=True)
-    weight = models.IntegerField('peso', null=True)
-    height = models.IntegerField('altura en cm', null=True)
+    phone = models.CharField(max_length=12, blank=True, null=True)
+    cant_km = models.IntegerField('cantidad de km', null=True,  blank=True)
+    weight = models.IntegerField('peso', null=True,  blank=True)
+    height = models.IntegerField('altura en cm', null=True,  blank=True)
 
-    #created_at = models.DateTimeField(auto_now_add=True)
-    #updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         """Return username's name."""
--- backend/AppWeb/bici/models.py
+++ backend/AppWeb/bici/models.py
@@ -4,12 +4,12 @@ class Store(models.Model):
 	class Meta:
 		verbose_name = 'tienda'
 	description = models.TextField('descripción', blank=True, null=True)
-	address = models.CharField('dirección', max_length=100)
-	image = models.ImageField('imagen', null=True)
-	value_km = models.IntegerField('cantidad de km', null=True)
+	address = models.CharField('dirección', max_length=100, blank=True, null=True)
+	image = models.ImageField('imagen', null=True, blank=True)
+	value_km = models.IntegerField('cantidad de km', blank=True, null=True)
 	
-	#created_at = models.DateTimeField(auto_now_add=True)
-	#updated_at = models.DateTimeField(auto_now=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
 	
 	def __str__(self):
 		return self.address
