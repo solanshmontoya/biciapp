90d3ec3b5e72d611bc3d61673f83fb69469aadfd
solansh <solanshmontoyamm@gmail.com>
2019-07-11 07:49:06 -0500

    lala

--- backend/AppWeb/bici/models.py
+++ backend/AppWeb/bici/models.py
@@ -5,7 +5,6 @@ class Store(models.Model):
 		verbose_name = 'tienda'
 	description = models.TextField('descripción', blank=True, null=True)
 	address = models.CharField('dirección', max_length=100, blank=True, null=True)
-	image = models.ImageField('imagen', null=True, blank=True)
 	value_km = models.IntegerField('cantidad de km', blank=True, null=True)
 	
 	created_at = models.DateTimeField(auto_now_add=True)
